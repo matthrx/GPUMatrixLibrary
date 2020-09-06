2f4bac562122a5fc37d37e3427db04bb7b080682
root <matthieu_upmc@outlook.fr>
2020-09-06 02:39:42 +0200

    update tests cpu

--- examples/cpu_example.cpp
+++ examples/cpu_example.cpp
@@ -16,33 +16,33 @@ int main(void){
     std::cout << "Time for CPU dot operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
-    std::chrono::steady_clock::time_point begin_measure = std::chrono::steady_clock::now();
+    begin_measure = std::chrono::steady_clock::now();
     arma::mat D = A+B;
-    std::chrono::steady_clock::time_point end_measure = std::chrono::steady_clock::now();
+    end_measure = std::chrono::steady_clock::now();
     std::cout << "Time for CPU sum operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
-    std::chrono::steady_clock::time_point begin_measure = std::chrono::steady_clock::now();
-    arma::mat C = arma::inv(A);
-    std::chrono::steady_clock::time_point end_measure = std::chrono::steady_clock::now();
+    begin_measure = std::chrono::steady_clock::now();
+    arma::mat E = arma::inv(A);
+    end_measure = std::chrono::steady_clock::now();
     std::cout << "Time for CPU inverse operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
-    std::chrono::steady_clock::time_point begin_measure = std::chrono::steady_clock::now();
-    double C = A.min();
-    std::chrono::steady_clock::time_point end_measure = std::chrono::steady_clock::now();
+    begin_measure = std::chrono::steady_clock::now();
+    double F = A.min();
+    end_measure = std::chrono::steady_clock::now();
     std::cout << "Time for CPU min operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
-    std::chrono::steady_clock::time_point begin_measure = std::chrono::steady_clock::now();
-    double D = B.max();
-    std::chrono::steady_clock::time_point end_measure = std::chrono::steady_clock::now();
+    begin_measure = std::chrono::steady_clock::now();
+    double G = B.max();
+    end_measure = std::chrono::steady_clock::now();
     std::cout << "Time for CPU max operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
-    std::chrono::steady_clock::time_point begin_measure = std::chrono::steady_clock::now();
-    auto E = arma::mean(C);
-    std::chrono::steady_clock::time_point end_measure = std::chrono::steady_clock::now();
+    begin_measure = std::chrono::steady_clock::now();
+    auto H = arma::mean(C);
+    end_measure = std::chrono::steady_clock::now();
     std::cout << "Time for CPU mean operation = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_measure - begin_measure).count() << "[ms]" << std::endl;
 
 
